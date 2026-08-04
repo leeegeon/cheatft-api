b4a1962a23f85da13414582d19b7e5100a4b5ff3
leeegeon <leegeon@yonsei.ac.kr>
2026-08-04 12:04:31 +0900

    fixed duplicates

--- src/routes/dummy.routes.js
+++ src/routes/dummy.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const DummyController = require('../controllers/dummy.controller');
 
-router.get('/summary', DummyController.getSummary);
 router.get('/posts', DummyController.getPosts);
 router.post('/posts', DummyController.createPost);
 
